9b379dd56db23918bc5631b886fc4e8f102cf702
LavrenyukAlex <almosk93@gmail.com>
2022-03-13 17:55:14 +0300

    navbar

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,36 +1,35 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <div class="container-fluid">
+            <div class="row ">
+                <div class="col-sm nav_adress">
+                    <span class="nav_city">г.Санкт-Петербург,</span><p> Софийская д.14</p>
+                </div>
+                <div class="col-md-6">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav_icons">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#"><i class="fa fa-youtube fa-3x" aria-hidden="true"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" tabindex="-1" aria-disabled="true"><i class="fa fa-vk fa-3x" aria-hidden="true"></i></a>
+                        </li>
                     </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+                </div>
+                <div class="col-md-auto nav_date">
+                    <span><b class="b_nav_date">8(812)980-58-66</b><br>Пн-Вс 09:00-21:00 </span>
+
+                </div>
+                <div class="col-md-auto">
+                    <div class="col text-center nav_button">
+                        @include('home.request-btn')
+                    </div>
+                </div>
+            </div>
         </div>
+
     </div>
 </nav>
