e2deb892d7cb0dd2b2f0edc886fee99874610583
Gorchel <kasatka567@gmail.com>
2022-03-14 20:49:25 +0300

    Advateges

--- resources/views/home/advantages.blade.php
+++ resources/views/home/advantages.blade.php
@@ -3,24 +3,26 @@
         <h2 class="text-center article-header">Преимущества</h2>
         <div class="row">
             <div class="col-6 col-md-4 advanteges-wrapper">
-                <div></div>
-                <div class="advanteges-block" style="background-image: url('/images/advanteges-one.png');">
-                    Бесплатный выезд
+                <div class="advanteges-block black">
+                    <i class="fa-solid fa-car-side fa-lg"></i>
+                    <p>Бесплатный выезд</p>
                 </div>
             </div>
             <div class="col-6 col-md-4 advanteges-wrapper">
-                <div class="advanteges-block" style="background-image: url('/images/advanteges-one.jpeg');">
-                    Без вреда для двигателя
+                <div class="advanteges-block black">
+                    <i class="fa-solid fa-gears fa-lg"></i>
+                    <p>Без вреда для двигателя</p>
                 </div>
             </div>
             <div class="col-6 col-md-4 advanteges-wrapper">
-                <div class="advanteges-block" style="background-image: url('/images/advanteges-one-one.png');">
-                    Снижение расхода топлива до 10%
+                <div class="advanteges-block black">
+                    <i class="fa-solid fa-gas-pump fa-lg"></i>
+                    <p>Снижение расхода топлива до 10%</p>
                 </div>
             </div>
             <div class="col-6 col-md-4 advanteges-wrapper">
                 <div class="advanteges-block" style="background-image: url('/images/advanteges-one-one.png')";>
-                    Увеличение мозности до 30%
+                    Увеличение мощности до 30%
                 </div>
             </div>
             <div class="col-6 col-md-4 advanteges-wrapper">
