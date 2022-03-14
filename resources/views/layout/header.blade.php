fc912a2fd63a5d86df7d0f8f8c96eabb79f7bdf2
LavrenyukAlex <almosk93@gmail.com>
2022-03-14 19:05:08 +0300

    first commit

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,34 +2,39 @@
     <div class="container-fluid">
         <div class="container-fluid">
             <div class="row ">
-                <div class="col-sm nav_adress">
-                    <span class="nav_city">г.Санкт-Петербург,</span><p> Софийская д.14</p>
+                <div class="col nav-address">
+                    <span class="nav-city">г.Санкт-Петербург,</span>
+                    <p class="nav-street"> Софийская д.14</p>
                 </div>
-                <div class="col-md-6">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav_icons">
+                <div class="col-sm-6">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-icons">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#"><i class="fa fa-youtube fa-3x" aria-hidden="true"></i></a>
+                            <a class="nav-link" aria-current="page" href="#"><i class="fa fa-youtube fa-2x"
+                                                                                aria-hidden="true"></i></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
+                            <a class="nav-link" href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" href="#" tabindex="-1" aria-disabled="true"><i class="fa fa-vk fa-3x" aria-hidden="true"></i></a>
+                            <a class="nav-link" href="#" tabindex="-1" aria-disabled="true"><i class="fa fa-vk fa-2x"
+                                                                                               aria-hidden="true"></i></a>
                         </li>
                     </ul>
                 </div>
-                <div class="col-md-auto nav_date">
-                    <span><b class="b_nav_date"><a href=""></a>8(812)980-58-66</b>Пн-Вс 09:00-21:00 </span>
+                <div class="col nav-date">
+                    <span><b class="b-nav-date"><a href=""></a>8(812)980-58-66</b><p>Пн-Вс 09:00-21:00 </p></span>
 
                 </div>
-                <div class="col-md-auto">
-                    <div class="col text-center nav_button">
+                <div class="col nav-button">
+
                         @include('home.request-btn')
-                    </div>
+
                 </div>
             </div>
         </div>
+
+
 
     </div>
 </nav>
