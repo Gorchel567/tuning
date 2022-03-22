00eeac2e565d401bee729178c7446ea9a7a66f65
Gorchel <kasatka567@gmail.com>
2022-03-22 18:27:36 +0300

    Add images

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/app.css">
-    <title>Document</title>
+    <link rel="icon" type="image/x-icon" href="/images/logo/favicon.ico">
+    <meta property="og:image" content="/images/logo/og_logo.png" />
+    <title>Chipograf</title>
 </head>
 <body>
     @include('layout.header')
