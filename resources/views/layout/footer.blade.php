537126f071e2ae01218941642c25270996b1a7e9
Gorchel <kasatka567@gmail.com>
2022-03-26 11:33:53 +0300

    Footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -10,9 +10,7 @@
                     @include('home.request')
                 </div>
             </div>
-            <div class="col-12 col-md-5 text-center">
-                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A7d99c761db467781e52bbabc55d9a12d1f5c98148346d78aff0f935ae04408d8&amp;width=100%25&amp;height=300&amp;lang=ru_RU&amp;scroll=true"></script>
-            </div>
+            <div class="col-12 col-md-5 text-center" id="map_container-one"></div>
         </div>
         <div class="row mg-t-25">
             <div class="col text-center">
@@ -21,3 +19,13 @@
         </div>
     </div>
 </footer>
+
+<script>
+    setTimeout(function () {
+        var elem = document.createElement('script');
+        elem.type = 'text/javascript';
+        elem.src = 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A7d99c761db467781e52bbabc55d9a12d1f5c98148346d78aff0f935ae04408d8&amp;width=100%25&amp;height=300&amp;lang=ru_RU&amp;scroll=true';
+        var div = document.getElementById('map_container-one');
+        div.append(elem);
+    }, 3000);
+</script>
